beb8d56954469cc30f59da4c1a3de170fc1e7dda
SenJamir <aosen.xeno@gmail.com>
2023-02-17 14:00:35 -0600

    step 1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
-import sys
+import sys  # functionality to make the game. Used to exit game when user quits
+
 import pygame
 
 
@@ -7,20 +8,23 @@ class AlienInvasion:
 
     def __init__(self):
         """Initialize the game, and create game resources."""
-        pygame.init()
+        pygame.init()   # Initialize background settings to run game
 
-        self.screen = pygame.display.set_mode((1200, 800))  # Screen dimensions
+        # Screen dimensions in self.screen for all methods in the class
+        self.screen = pygame.display.set_mode((1200, 800))
+        # display.set_mode represents the entire game window
         pygame.display.set_caption("Alien Invasion")  # Game title
 
     def run_game(self):
         """Start the main loop for the game."""
-        while True:
-            # Watch for keyboard and mouse events.
+        while True:     # While loop to run the game continually
+            # Watch for keyboard and mouse events (keystroke or mouse movement)
             for even in pygame.event.get():
-                if even.type == pygame.QUIT:
+                if even.type == pygame.QUIT:    # Quits the game if user selects quit
                     sys.exit()
 
             # Make the most recently drawn screen visible.
+            # Deletes old screen and updates new screen with new positions depending on events
             pygame.display.flip()
 
 
