a9010fced8f9b93b10ffc31c1d4f403e9ad24e01
SenJamir <aosen.xeno@gmail.com>
2023-03-01 20:18:55 -0600

    high score

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -28,9 +29,9 @@ class AlienInvasion:
         # display.set_mode represents the entire game window
         pygame.display.set_caption("Alien Invasion")  # Game title
 
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics and create a scoreboard
         self.stats = GameStats(self)
-
+        self.sb = Scoreboard(self)
         # gives ship access to the game's resources, such as the screen object
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -118,10 +119,18 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            # self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
@@ -132,6 +141,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
@@ -232,8 +244,11 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             # Reset the game statistics.
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
