c70a8ed97fa972232982c56fb7565d309a8265af
SenJamir <aosen.xeno@gmail.com>
2023-03-01 18:45:44 -0600

    added start button

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -36,6 +37,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # Make the Play button.
+        self.play_button = Button(self, "Play")
 
         # Set the background color
         self.bg_color = (230, 230, 230)  # RGB colors that create a light grey
@@ -125,6 +129,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # Deletes old screen and updates new screen with new positions depending on events
         pygame.display.flip()
